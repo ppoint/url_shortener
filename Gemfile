3ae93f4d0f1b95e01ce3fc4b627c59d614a01fa0
Paul Walrath <paul@walrath.ca>
2024-10-13 22:33:25 -0400

    The beginnings of a plan

--- Gemfile
+++ Gemfile
@@ -4,6 +4,16 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.3.5"
 
 gem "sinatra"
+gem "sinatra-contrib"
 gem "rackup"
 gem "puma"
 gem "rspec"
+
+group :development, :test do
+  gem "rerun"
+end
+
+
+# Standard library deprecation warnings
+gem "ostruct"
+gem "logger"
--- url_shortener.rb
+++ url_shortener.rb
@@ -1,21 +1,27 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'sinatra/json'
+require 'json'
 
 post '/encode' do
   request.body.rewind
   data = JSON.parse(request.body.read)
   if (data['url'])
-    asdf
+    result = {
+      :url => data['url'],
+      :short_url => 'asdf',
+    }
+    json result
   else
-    error
+    json :error => "POST is missing JSON field 'url'"
   end
 end
 
 post '/decode' do
-  asdf
+  json :error => "not implemented yet"
 end
 
 get '/' do
-  error "route not implemented - URL Shortener service"
+  json :error => "route not implemented - URL Shortener service"
 end
