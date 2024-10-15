cebfdb382980048a09ba725f977016411e5eb117
Paul Walrath <paul@walrath.ca>
2024-10-14 23:08:57 -0400

    Testing HTTP POST

--- lib/url_shortener.rb
+++ lib/url_shortener.rb
@@ -2,20 +2,30 @@ require 'sinatra'
 require 'sinatra/json'
 require 'json'
 
+BASE_SHORT_URL = "http://ex.eg"
+
 post '/encode' do
-  request.body.rewind
+  content_type :json
+
   data = JSON.parse(request.body.read)
   if (data['url'])
+    short_url = make_short_url(data['url'])
     result = {
       :url => data['url'],
-      :short_url => 'asdf',
+      :short_url => short_url,
     }
+    # @TODO when we add DB component, we'll need to handle cases where a URL has already been shortened
     json result
   else
-    json :error => "POST is missing JSON field 'url'"
+    status 400
+    { error: "POST is missing JSON field 'url'" }.to_json
   end
 end
 
 post '/decode' do
   json :error => "not implemented yet"
+end
+
+def make_short_url(url)
+  "#{BASE_SHORT_URL}/"
 end
--- spec/encode_spec.rb
+++ spec/encode_spec.rb
@@ -1,21 +1,39 @@
 require_relative 'spec_helper'
 
+HEADERS = { 'CONTENT_TYPE' => 'application/json' }
+
+
 def app
   Sinatra::Application
 end
 
 RSpec.describe 'POST /encode' do
 
-  it "receives" do
-    expect(false).to be_true
+  context "receives JSON with 'url' as the only required parameter" do
+    it "converts 'url' to a short-url" do
+      post '/'
+    end
+
   end
 
+  context "receives JSON, but required parameter is missing" do
+    it "produces a HTTP 400 'Bad Request' message" do
+      payload = { foo: 'bar' }.to_json
+
+      post '/encode', payload, HEADERS
+
+      expect(last_response.status).to eq(400)
+      response_body = JSON.parse(last_response.body)
+      expect(response_body['error']).to eq("POST is missing JSON field 'url'")
+    end
+
+  end
 end
 
 RSpec.describe 'unsupported endpoints' do
   it "handles any request to endpoints that aren't recognized" do
     get '/'
-    expect(last_response).not_to be_ok
+    expect(last_response).to be_not_found
   end
 
   it "doesn't support GET /encode" do
